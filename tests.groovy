c93564be9e124ec667c1f25c69563b91d74b7382
Michał Grzech <michalgrzech93@gmail.com>
2024-01-06 12:09:04 +0100

    test empty groovy file

--- tests.groovy
+++ tests.groovy
@@ -1,18 +1,6 @@
 def runTest(expectedJson, testNumber) {
     println "Performing ${testNumber} test."
-    def expected = new groovy.json.JsonSlurper().parseText(expectedJson)
-
-    def responseJson = sh(script: "curl -s -X POST http://localhost:8888/api/numbers", returnStdout: true).trim()
-    def response = new groovy.json.JsonSlurper().parseText(responseJson)
-
-    response.each { it.remove('creationTime') }
-
-    if (expected != response) {
-        println "Test number ${testNumber} failed."
-        error("JSON are not the same! Expected: ${expectedJson}, recieved: ${responseJson}")
-    } else {
-        println "Test number ${testNumber} passed."
-    }
+    
 
     return true
 }
