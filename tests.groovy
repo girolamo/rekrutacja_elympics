10a58595b47d54467d09a8835526e757f62efe40
Michał Grzech <michalgrzech93@gmail.com>
2024-01-06 11:52:31 +0100

    refactor tests groovy one more time

--- tests.groovy
+++ tests.groovy
@@ -1,18 +1,23 @@
 def runTest(String expectedJson, int testNumber) {
-    sh script: '''
-        response=$(curl -X POST http://localhost:8888/api/numbers)
-        processed_response=$(echo $response | jq -c '[.[] | {value: .value}]')
-        
-        expected_normalized=$(echo $''' + expectedJson + ''' | jq -c .)
-        response_normalized=$(echo $processed_response | jq -c .)
+    if (!sh(script: 'command -v jq', returnStatus: true)) {
+        echo 'jq is not installed. Test cannot be run.'
+        return
+    }
 
-        if [ "$response_normalized" = "$expected_normalized" ]; then
-            echo "''' + testNumber + '''. TEST PASSED"
+    script = """
+        response=\$(curl -s -X POST http://localhost:8888/api/numbers)
+        processed_response=\$(echo \$response | jq -c '[.[] | {value: .value}]')
+
+        expected_normalized=\$(echo ${JsonOutput.toJson(expectedJson)} | jq -c .)
+        response_normalized=\$(echo \$processed_response | jq -c .)
+
+        if [ "\$response_normalized" = "\$expected_normalized" ]; then
+            echo "${testNumber}. TEST PASSED"
         else
-            echo "''' + testNumber + '''. TEST FAILED: Expected $expected_normalized, Recieved $response_normalized"
+            echo "${testNumber}. TEST FAILED: Expected \$expected_normalized, Received \$response_normalized"
             exit 1
         fi
-    ''', returnStatus: false
-}
+    """
 
-return this
+    sh(script: script, returnStatus: false)
+}
