b66ded8da5382d64fb267229ea4e4c1bcdcca0dc
Michał Grzech <michalgrzech93@gmail.com>
2024-01-06 11:34:49 +0100

    fix groovy test

--- tests.groovy
+++ tests.groovy
@@ -2,10 +2,14 @@ def runTest(String expectedJson, int testNumber) {
     sh script: '''
         response=$(curl -X POST http://localhost:8888/api/numbers)
         processed_response=$(echo $response | jq -c '[.[] | {value: .value}]')
-        if [ "$processed_response" = "$''' + expectedJson + '''" ]; then
+        
+        expected_normalized=$(echo ''' + expectedJson + ''' | jq -c .)
+        response_normalized=$(echo $processed_response | jq -c .)
+
+        if [ "$response_normalized" = "$expected_normalized" ]; then
             echo "''' + testNumber + '''. TEST PASSED"
         else
-            echo "''' + testNumber + '''. TEST FAILED: Oczekiwano ''' + expectedJson + ''', otrzymano $processed_response"
+            echo "''' + testNumber + '''. TEST FAILED: Expeceted $expected_normalized, recieved $response_normalized"
             exit 1
         fi
     ''', returnStatus: false
